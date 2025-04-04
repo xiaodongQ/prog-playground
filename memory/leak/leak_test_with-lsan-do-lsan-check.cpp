ce2cefbdb7e7b9e0fb12f6244fb60c071fbe2ba1
xiaodongQ <xiaodong0795@163.com>
2025-04-05 06:53:39 +0800

    ThreadSanitizer

--- memory/leak/leak_test_with-lsan-do-lsan-check.cpp
+++ memory/leak/leak_test_with-lsan-do-lsan-check.cpp
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <time.h>
 #include <pthread.h>
+#include <sanitizer/lsan_interface.h>
 
 #define MAX_CHUNK_SIZE 1024 * 1024 // 最大块大小为 1 MB
 #define MIN_CHUNK_SIZE 1024        // 最小块大小为 1 KB
@@ -104,8 +105,11 @@ int main(int argc, char *argv[]) {
 
     // 等待泄漏线程完成
     pthread_join(leak_thread, NULL);
-
     printf("\nAll Tests Done.\n");
+
+    // 手动触发内存泄漏检查，避免手动ctrl+c打断下面的while循环时无法触发检查
+    // 实际项目中可添加条件编译宏
+    __lsan_do_leak_check();
 
     // 主动进入无限循环，方便观察内存占用情况
     while (1) {
